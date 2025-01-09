04cc9adae80d7d77cd7f387a1ec4562f162428b9
alicansadeler <alicansadeler@gmail.com>
2025-01-09 17:06:48 +0300

    1

---
+++ src/ArrayString/MergeString.java
@@ -0,0 +1,31 @@
+package ArrayString;
+/*
+İki string veriliyor: word1 ve word2
+Bu stringleri sırayla karakterleri alarak birleştirmemiz gerekiyor
+Örneğin:
+
+word1 = "abc", word2 = "pqr" ise
+Sonuç: "apbqcr" olmalı
+
+
+
+Çözüm stratejisi:
+
+İki string'in karakterlerini sırayla alacağız
+İki pointer kullanabiliriz (two pointers yaklaşımı)
+Eğer bir string biterse, diğer string'in kalan karakterlerini ekleriz
+ */
+public class MergeString {
+    public String mergeAlternately(String word1, String word2) {
+        StringBuilder result = new StringBuilder(word1.length() + word2.length());
+        int i = 0;
+        int length = Math.max(word1.length(), word2.length());
+
+        while (i < length ) {
+            if (i < word1.length()) result.append(word1.charAt(i));
+            if (i < word2.length()) result.append(word2.charAt(i));
+            i++;
+        }
+        return result.toString();
+    };
+}
